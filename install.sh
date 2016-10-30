3c4b936104fa8685801f8e267dd8c354418fceb4
Kareem El-Faramawi <kareem02@gmail.com>
2016-10-30 16:43:06 -0400

    Add simple installer

--- install.sh
+++ install.sh
@@ -1,14 +1,68 @@
 #!/bin/bash
 
-DOTSDIR="$HOME/dotfiles"
-cd $DOTSDIR
+DIR=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
 
-GLOBIGNORE=".:..:.git:$(basename $0):util:zsh"
-shopt -s dotglob
+function link_home {
+    ln -s "$DIR/$1" "$HOME/.$1"
+}
 
-for f in $(echo *)
-do
-    LINK=$HOME/$f
-    if [ ! -d $LINK  ]; then ln -s $DOTSDIR/$f $LINK; fi
+function setup_vim {
+    # Setup plug
+    mkdir -p "$HOME/.vim/autoload"
+    curl -fLo ~/.vim/autoload/plug.vim https://raw.github.com/junegunn/vim-plug/master/plug.vim
+
+    link_home vimrc
+}
+
+function setup_zsh {
+    if [[ ! -d "$HOME/.zprezto" ]]; then
+        git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+        zsh -c 'setopt EXTENDED_GLOB; for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"; done'
+    fi
+
+    cp "$DIR/zsh/prompt_krx_setup" "$HOME/.zprezto/modules/prompt/functions"
+    cp -r "$DIR/zsh/runcoms" "$HOME/.zprezto"
+}
+
+function setup_git {
+    link_home gitconfig
+}
+
+function setup_gdb {
+    if [[ ! -d ~/tools/peda ]]; then
+        git clone https://github.com/longld/peda ~/tools/peda
+    fi
+    link_home gdbinit
+}
+
+function setup_xinit {
+    link_home xinitrc 
+}
+
+function setup_tmux {
+    link_home tmux.conf 
+}
+
+function setup_i3 {
+    mkdir -p ~/.config/i3
+    ln -s i3config ~/.config/i3/config 
+}
+
+
+for i in "$@"; do
+    if [[ "$(type -t setup_$i)" == "function" ]]; then
+        setup_$i
+    else
+        echo "Unknown command $i"
+    fi
 done
+
+#GLOBIGNORE=".:..:.git:$(basename $0):util:zsh"
+#shopt -s dotglob
+
+#for f in $(echo *)
+#do
+    #LINK=$HOME/$f
+    #if [ ! -d $LINK  ]; then ln -s $DIR/$f $LINK; fi
+#done
 
