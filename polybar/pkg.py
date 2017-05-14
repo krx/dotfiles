e47e07b81a53d38fd3b9a72f5a27c014af8aef32
Kareem El-Faramawi <kareem02@gmail.com>
2017-05-14 07:44:40 -0400

    Update polybar pkg module

---
+++ polybar/pkg.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# encoding: utf-8
+import sh
+
+def cnt(cmd):
+    try:
+        return int(sh.wc(cmd(_ok_code=xrange(256)), '-l').stdout)
+    except:
+        return 0
+
+print '{} %{{F#b16286}}%{{F-}} {}'.format(
+    cnt(sh.checkupdates),
+    cnt(sh.cower.bake('-u'))
+)
+
--- polybar/pkg.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-pac=$(checkupdates | wc -l)
-aur=$(cower -u | wc -l)
-echo "$pac %{F#b16286}%{F-} $aur"
-
