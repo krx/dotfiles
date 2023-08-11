a41e9e432619e1d11fe6cfb9ebd1323103145cf9
Kareem ElFaramawi <kareem.elfaramawi@oracle.com>
2023-08-11 09:53:16 -0400

    update indent highlight

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,4 +1,14 @@
 return {
   { "lambdalisue/suda.vim" },
   { "HiPhish/rainbow-delimiters.nvim" },
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      draw = {
+        animation = function(s, n)
+          return 10
+        end,
+      },
+    },
+  },
 }
