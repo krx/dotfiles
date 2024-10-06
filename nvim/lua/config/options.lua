876ea60a099d6a5c3f28eceb04af5021a41ac42f
krx <kareem02@gmail.com>
2024-10-06 19:06:44 -0400

    some vim options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -10,10 +10,13 @@ vim.o.visualbell = true
 vim.o.termguicolors = true
 vim.o.mouse = "a"
 vim.o.foldmethod = "marker"
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
 vim.g.autoformat = false
 
 -- disable comments going to next line
-vim.o.formatoptions = vim.o.formatoptions:gsub("[crv]", "")
+-- vim.o.formatoptions = vim.o.formatoptions:gsub("[crv]", "")
+vim.o.formatoptions = "tcqj"
 
 if vim.fn.has("gui_macvim") then
   vim.g.fu = true
@@ -25,3 +28,25 @@ vim.o.smartcase = true
 --
 -- vim.g.lazyvim_python_ruff = "ruff_lsp"
 -- vim.g.lazyvim_python_lsp = ""
+
+local function paste()
+  return {
+    vim.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+if vim.env.SSH_TTY then
+  vim.o.clipboard = "unnamedplus"
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+end
