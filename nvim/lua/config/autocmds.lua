033019d5b253b69377dc2ba6ff94701bc3609920
Kareem ElFaramawi <kareem02@gmail.com>
2024-10-16 14:43:06 -0400

    fix folding settings

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -40,3 +40,14 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.formatoptions:remove('o')
   end,
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  callback = function()
+    if require("nvim-treesitter.parsers").has_parser() then
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    else
+      vim.opt.foldmethod = "syntax"
+    end
+  end,
+})
