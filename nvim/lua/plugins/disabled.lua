93843a71ae757b44b089362db0625b44c44e8fcc
krx <kareem02@gmail.com>
2024-11-19 10:11:29 -0500

    update nvim

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,10 @@
 return {
   { "goolord/alpha-nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = { enabled = false },
+    },
+  },
 }
